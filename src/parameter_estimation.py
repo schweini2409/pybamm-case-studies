1dce12a15e75c3da6dcb5804a507e5f0747f4f05
Valentin Sulzer <valentinsulzer@hotmail.com>
2020-04-02 16:49:48 -0400

    add least-squares parameter estimation

--- src/parameter_estimation.py
+++ src/parameter_estimation.py
@@ -7,6 +7,7 @@ import dfols
 import pybamm
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import least_squares
 
 # pybamm.set_logging_level("INFO")
 model = pybamm.lithium_ion.DFN()
@@ -54,6 +55,7 @@ true_solution = solver.solve(
 )
 data = objective(true_solution)
 # add random normal noise
+np.random.seed(42)
 data_plus_noise = data + 0.01 * np.random.normal(size=data.shape[0]) / np.max(data)
 
 
@@ -78,21 +80,34 @@ def prediction_error(x):
         return 1e5 * np.ones_like(data_plus_noise)
     else:
         out = prediction - data_plus_noise
-        print(x, np.linalg.norm(out))
+        # print(x, np.linalg.norm(out))
         return out
 
 
 # Do parameter fitting to find solution (using derivative-free library)
 x0 = np.array([0.7, 0.5])
-soln = dfols.solve(prediction_error, x0)  # , bounds=(np.array([0]), None))
-found_solution = solver.solve(
-    model,
-    t_eval,
-    inputs={
-        "Cation transference number": soln.x[0],
-        "Electrolyte conductivity [S.m-1]": soln.x[1],
-    },
-)
+
+soln_lsq = least_squares(prediction_error, x0, verbose=2)
+soln_dfols = dfols.solve(prediction_error, x0)  # , bounds=(np.array([0]), None))
+
+for algorithm, soln in [("scipy.least_squares", soln_lsq), ("DFO-LS", soln_dfols)]:
+    print(algorithm)
+    print("-" * 20)
+    print(soln)
+    print("-" * 20)
+    found_solution = solver.solve(
+        model,
+        t_eval,
+        inputs={
+            "Cation transference number": soln.x[0],
+            "Electrolyte conductivity [S.m-1]": soln.x[1],
+        },
+    )
+    plt.plot(
+        found_solution["Time [h]"].entries,
+        found_solution["Terminal voltage [V]"].entries,
+        label=algorithm,
+    )
 
 # Generate initial guess, for plotting
 init_solution = solver.solve(
@@ -106,11 +121,6 @@ init_solution = solver.solve(
 
 # Plot
 plt.plot(true_solution["Time [h]"].entries, data_plus_noise, label="data")
-plt.plot(
-    found_solution["Time [h]"].entries,
-    found_solution["Terminal voltage [V]"].entries,
-    label="fit",
-)
 plt.plot(
     init_solution["Time [h]"].entries,
     init_solution["Terminal voltage [V]"].entries,
